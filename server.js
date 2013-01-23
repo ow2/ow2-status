a72b479a1d0f256f53a104e51a8a300b8baa07b2
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-01-23 23:26:21 +0100

    initial version

---
+++ server.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Copyright(c) 2013 Christophe Hamerling <chamerling@ow2.org>
+ * MIT Licensed
+ */
+ 
+var fs = require('fs')
+  , util = require('util')
+  , server = require('statusdashboard/server')
+
+var input = fs.readFileSync('./config.json', 'utf-8');
+var settings = JSON.parse(input);
+
+// update settings from platform variables
+settings.port = process.env.PORT || settings.port;
+// settings REDISTOGO URL
+// TODO
+
+process.on('exit', function () {
+  util.log('Bye bye Statusdashboard.');
+});
+
+process.on('uncaughtException', function(err) {
+  util.log(err.stack.replace(/^    /gm, '                  '));
+});
+
+/* Not available in npmjs for now
+var dashboard = new server.dashboard(settings, function() {
+  console.log('Server is started!!!');
+  console.log(settings)
+});
+*/
+var dashboard = new server.dashboard(settings);
